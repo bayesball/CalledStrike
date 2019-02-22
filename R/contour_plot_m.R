4517913691f933fdc70fc1811dca76d8f50e949d
Jim Albert <albertcb1@gmail.com>
2019-02-21 22:31:52 -0500

    added documentation

---
+++ R/contour_plot_m.R
@@ -0,0 +1,26 @@
+contour_plot_m <- function(fit, M){
+  require(ggplot2)
+  require(mgcv)
+  df_p <- expand.grid(plate_x = seq(-1.5, 1.5, length=50),
+                      plate_z = seq(1, 4, length=50))
+  df_p$lp <- predict(fit, df_p)
+  topKzone <- 3.5
+  botKzone <- 1.6
+  inKzone <- -0.85
+  outKzone <- 0.85
+  kZone <- data.frame(
+    x=c(inKzone, inKzone, outKzone, outKzone, inKzone),
+    y=c(botKzone, topKzone, topKzone, botKzone, botKzone)
+  )
+  ggplot(df_p)  +
+    stat_contour(aes(x=plate_x, y=plate_z,
+                     z=lp,
+                     color = stat(level)),
+                 breaks=c(M),
+                 size=1.5) +
+    geom_path(aes(x, y), data=kZone,
+              lwd=1, col="red") +
+    xlim(-1.5, 1.5) +
+    ylim(1.0, 4.0)  +
+    coord_fixed()
+}
