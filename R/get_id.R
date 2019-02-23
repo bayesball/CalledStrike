c47b8efb9e9de074957c3d69cf8730cd51e3a95b
Jim Albert <albertcb1@gmail.com>
2019-02-23 12:21:19 -0500

    adjust sc functions

--- R/get_id.R
+++ R/get_id.R
@@ -1,5 +1,4 @@
 get_id <- function(name){
-  require(baseballr)
   names <- unlist(strsplit(name, " "))
   playerid_lookup(last_name = names[2],
                   first_name = names[1]) %>%
--- R/get_sc_data.R
+++ R/get_sc_data.R
@@ -1,9 +1,15 @@
-get_sc_data <- function(id, season = 2018){
-  require(baseballr)
+get_sc_data <- function(id, season = 2018,
+                        batter = TRUE){
   dstart <- paste(season, "-03-15", sep="")
   dstop <- paste(season, "-10-15", sep="")
+  if(batter == TRUE){
   scrape_statcast_savant(start_date = dstart,
                                end_date = dstop,
                                player_type = "batter",
-                               playerid = id)
+                               playerid = id)} else {
+  scrape_statcast_savant(start_date = dstart,
+                               end_date = dstop,
+                               player_type = "pitcher",
+                               playerid = id)}
+
 }
