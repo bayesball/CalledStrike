63671872ff65cbfab247151a9c47c968d8b3b9d8
Jim Albert <albertcb1@gmail.com>
2020-11-26 10:00:19 -0500

    add two functions

---
+++ R/setup_LR.R
@@ -0,0 +1,3 @@
+setup_LR <- function(sc){
+  split(sc, sc$p_throws)
+}
---
+++ R/setup_pitchtype.R
@@ -0,0 +1,12 @@
+setup_pitchtype <- function(sc){
+  sc %>% mutate(Pitch_Type =
+                  ifelse(pitch_type %in%
+                        c("FC", "FF", "FS", "FT", "SI"),
+                         "Fastball",
+                         ifelse(pitch_type %in%
+                        c("CH", "CU", "KC", "SL"),
+                                "Offspeed", "Other")))  %>%
+    filter(Pitch_Type != "Other") -> sc
+
+  split(sc, sc$Pitch_Type)
+}
